a9d63563f97bcdec77f1268977759a6e50defdc5
Max Neiheisel <maxneiheisel@Max-Neiheisels-MacBook-2.local>
2017-04-11 20:09:19 -0400

    styled and weather

--- js/services/API.js
+++ js/services/API.js
@@ -3,6 +3,16 @@
 
     angular
         .module('routing')
-        
+        .factory('API', function($http){
+
+        return {
+        	getWeather:(city) => {
+		    	return $http({
+		    		type:"GET",
+		    		url:`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=7cf16558d759d14815306832bd7341e2&units=imperial`,
+		    	})
+		    }
+    	}
+    });
 
 })();
